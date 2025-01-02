f7502b6f5d723f0fb40b27b79da91587a51f1ccb
Eason <yisyuanshen@gmail.com>
2025-01-02 16:59:31 +0800

    Add corgi_impedance_control package

---
+++ src/corgi_impedance_control/include/force_estimation.hpp
@@ -0,0 +1,32 @@
+#ifndef FORCE_ESTIMATE_HPP
+#define FORCE_ESTIMATE_HPP
+
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+#include "leg_model.hpp"
+
+#include "ros/ros.h"
+#include "corgi_msgs/MotorStateStamped.h"
+#include "corgi_msgs/ForceStateStamped.h"
+
+
+corgi_msgs::MotorStateStamped motor_state;
+corgi_msgs::ForceStateStamped force_state;
+
+Eigen::MatrixXd H_l_poly(2, 8);
+Eigen::MatrixXd U_l_poly(2, 8);
+Eigen::MatrixXd F_l_poly(2, 8);
+Eigen::MatrixXd L_l_poly(2, 8);
+Eigen::MatrixXd G_poly(2, 8);
+Eigen::MatrixXd L_r_poly(2, 8);
+Eigen::MatrixXd F_r_poly(2, 8);
+Eigen::MatrixXd U_r_poly(2, 8);
+Eigen::MatrixXd P_poly(2, 8);
+
+void motor_state_cb(const corgi_msgs::MotorStateStamped state);
+Eigen::MatrixXd calculate_P_poly(int rim, double alpha);
+Eigen::MatrixXd calculate_jacobian(Eigen::MatrixXd P_theta, Eigen::MatrixXd P_theta_deriv, double beta);
+Eigen::MatrixXd estimate_force(double theta, double beta, double torque_r, double torque_l);
+#endif
